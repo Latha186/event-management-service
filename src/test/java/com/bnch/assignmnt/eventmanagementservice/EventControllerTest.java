a40682f07249687e704a77ee26f02fdeda5cedb2
latha186 <latha186@gmail.com>
2024-01-29 17:55:40 +0530

    added test class

--- src/test/java/com/bnch/assignmnt/eventmanagementservice/EventControllerTest.java
+++ src/test/java/com/bnch/assignmnt/eventmanagementservice/EventControllerTest.java
@@ -1,11 +1,33 @@
 package com.bnch.assignmnt.eventmanagementservice;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import com.bnch.assignmnt.eventmanagementservice.controller.EventController;
+import com.bnch.assignmnt.eventmanagementservice.entity.Event;
+import com.bnch.assignmnt.eventmanagementservice.service.EventService;
 
-@RunWith(SpringRunner.class)
+
+import org.junit.jupiter.api.Test;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 @SpringBootTest
 public class EventControllerTest {
-    
+    @InjectMocks
+    EventController eventController;
+    @Mock
+    EventService eventService;
+
+    @Test
+    public void testCreateEvent() throws Exception{
+        Event eventRes = new Event();
+        Mockito.when(eventService.createEvent(new Event())).thenReturn(eventRes);
+        assertNotNull(eventController.createEvent(new Event()));
+    }
 }
+
+
